bac32fd982495634fa319f0ba38161a47ee9be01
Anthony Sherwood <iworkinpixels@gmail.com>
2014-03-24 10:30:43 -0500

    Max Flow
    Adds max flow as well as min flow, and changes the name of the
    constants slightly.

--- flowmeter.py
+++ flowmeter.py
@@ -4,7 +4,8 @@ class FlowMeter():
   PINTS_IN_A_LITER = 2.11338
   SECONDS_IN_A_MINUTE = 60
   MS_IN_A_SECOND = 1000.0
-  MINIMUM_HZ = 0.25 # Minimum rate of clicks, in Hz, that we are willing to count as an actual flow
+  MIN_HZ = 0.25 # Minimum rate of clicks, in Hz, that we are willing to count as an actual flow
+  MAX_HZ = 80 # Maximum rate of clicks, in Hz, that we are willing to count as an actual flow
   displayFormat = 'metric'
   beverage = 'beer'
   enabled = True
@@ -35,7 +36,7 @@ class FlowMeter():
     # calculate the instantaneous speed
     if (self.enabled == True and self.clickDelta < 1000):
       self.hertz = FlowMeter.MS_IN_A_SECOND / self.clickDelta
-      if (self.hertz > FlowMeter.MINIMUM_HZ) # Only update the flow is fast enough, to avoid electrical shenanigans by the flow meters.
+      if (self.hertz > FlowMeter.MIN_HZ and self.hertz < FlowMeter.MAX_HZ) # Only update the flow is fast enough, to avoid electrical shenanigans by the flow meters.
         self.flow = self.hertz / (FlowMeter.SECONDS_IN_A_MINUTE * 7.5)  # In Liters per second
         instPour = self.flow * (self.clickDelta / FlowMeter.MS_IN_A_SECOND)  
         self.thisPour += instPour
